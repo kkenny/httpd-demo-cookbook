652bb6faf55be11dc1977c43e615acf8db2f3e8f
Kameron Kenny <kkenny@users.noreply.github.com>
2025-12-07 13:57:04 -0500

    Add default attributes for httpd, update the recipe, and bump the cookbook version.

---
+++ attributes/default.rb
@@ -0,0 +1,5 @@
+default['httpd']['html_dir']      = '/var/www/html'
+default['httpd']['package_name']  = 'httpd'
+default['httpd']['service_name']  = 'httpd'
+default['httpd']['user']          = 'apache'
+default['httpd']['group']          = 'apache'
--- recipes/default.rb
+++ recipes/default.rb
@@ -5,20 +5,20 @@
 # Copyright:: 2025, The Authors, All Rights Reserved.
 
 # Install httpd package
-package 'httpd' do
+package node['httpd']['package_name'] do
   action :install
 end
 
 # Enable httpd service to start on boot
-service 'httpd' do
+service node['httpd']['service_name'] do
   action [:enable, :start]
 end
 
 # Create index.html file
-file '/var/www/html/index.html' do
+file "#{node['httpd']['html_dir']}/index.html" do
   content '<html><body><h1>Hello World</h1></body></html>'
-  owner 'apache'
-  group 'apache'
+  owner node['httpd']['user']
+  group node['httpd']['group']
   mode '0644'
   action :create
 end
